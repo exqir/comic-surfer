5208ac569fd42bf41eeef952b372f51e06905b1c
Malte Sellmer <malte.sellmer@me.com>
2020-08-10 21:24:48 +0200

    change debug api route for atlas function

--- packages/app/src/pages/api/logOrigin.ts
+++ packages/app/src/pages/api/logOrigin.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('Incoming request from', req.headers.referer)
-  res.json({ from: req.headers.referer })
+  console.log('Incoming request from', req.headers.origin)
+  res.json({ from: req.headers })
 }
